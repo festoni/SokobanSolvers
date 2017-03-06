a93af89e39695a5f3af0c29c80278781c3f6c643
Feston Kastrati <fkastrati13@simons-rock.edu>
2017-03-06 03:37:24 -0500

    adding A star with pruning

--- astar.py
+++ astar.py
@@ -11,7 +11,6 @@ from heuristics import boxes, manhattan
 #verbose boolean wether to print solution path or not
 #returns the solution path or None if no solution found
 def Astar(start, goals, walls, heuristic, verbose=False):
-
     #frontier will look like [(f(path), [path]), (f(path), [path]), ...]
     frontier = []
     heappush(frontier, (0, [start]))    #heap prioritized by f()
@@ -39,12 +38,68 @@ def Astar(start, goals, walls, heuristic, verbose=False):
             heappush(frontier, new_tup)                 #prioritize by f()
     return None
 
+#A* search with pruning
+#takes in start matrix, goals matrix, walls matrix, heuristic function, and the
+#verbose boolean wether to print solution path or not
+#returns the solution path or None if no solution found
+def Astar2(start, goals, walls, heuristic, verbose=False):
+
+    #find permutation of start matrix and get information that is used in
+    #ranking and unranking
+    #max_pot is the total number of permutations for this multiset
+    #length is the length of the multiset
+    #type_count is a list of counts for each type in the sorted multiset
+    start_perm = to_perm(start)                 #find perm of start matrix
+    max_pot, length, type_count = get_info(start_perm)  #get level info
+
+    #get rank of start matrix
+    start_int = rank(start_perm, max_pot, length, type_count)
+
+    frontier = []                               #initiliaze frontier
+    heappush(frontier, (0,[start_int]))         #add (0, index) tuple
+
+    is_visited = [False for i in range(max_pot)]    #initiliaze visited array
+    is_visited[start_int] = True                    #mark start as visited
+
+    while frontier:
+        path_tup = heappop(frontier)            #take first path tuple
+        last_vertex = path_tup[1][-1]           #take last element of path
+
+        #unrank to multiset, and convert to matrix to check for goal and neighbs
+        last_permut = unrank(last_vertex, max_pot, length, type_count)
+        last_matrix = to_matrix(last_permut, walls)
+
+        if is_goal(last_matrix, goals):         #if boxes match goals, finish
+            if verbose:                         #if asked print solution path
+                print(path_tup[1])
+            print("cost\t:", path_tup[0])       #print cost for convinience
+            print("length\t:", len(path_tup[1]))    #print length for conv
+            return path_tup
+        for next_tup in neighbors(last_matrix, with_cost=True): #for each neighb
+            future_cost = heuristic(next_tup[1], goals) #calculate future cost
+
+            #convert from matrix to permutation, then to index
+            temp_perm = to_perm(next_tup[1])
+            int_next = rank(temp_perm, max_pot, length, type_count)
+
+            if is_visited[int_next]:            #if already visited skip
+                continue
+
+
+            combined_cost = next_tup[0] + future_cost   #calculate f()
+            new_tup = (path_tup[0] + combined_cost, path_tup[1] + [int_next])
+            is_visited[int_next] = True                 #set new int to visited
+            heappush(frontier, new_tup)                 #prioritize by f()
+    return None
+
 
 def main():
     start_time = time()
 
     test, goals, walls = read()
-    Astar(test, goals, walls, manhattan)      #no pruning
+    Astar(test, goals, walls, manhattan)        #no pruning
+    Astar2(test, goals, walls, manhattan)       #with pruning
+
 
     print("--- %s seconds ---" % (time() - start_time))
 
