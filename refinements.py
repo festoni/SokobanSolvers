bd8f14fb97adc4c6ba39e59fd9d3da80e792783b
Feston Kastrati <fkastrati13@simons-rock.edu>
2017-03-09 22:31:13 -0500

    changes and documentation

--- refinements.py
+++ refinements.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 from read import read
 
+'''
+>>Box in corner<<
+Input: State matrix, goals matrix, x index of player, y index of player
+Output: Boolean if box is in corner that is not goal
+'''
 def is_cornered(state, goals, j, k):
     if goals[j][k] == True:                     #if box on goal, return false
         return False
@@ -18,11 +23,30 @@ def is_cornered(state, goals, j, k):
             return True
     return False
 
+'''
+>>Boxes stuck<<
+Input: Goals in borders list, boxes in borders list, change variable
+Output: boolean if boxes are stuck
+This function takes in the following parameters where the change variable
+specifies at which border was a boxed move into. Goals list and boxes list are
+both length 4, where ls[0] is count for goals, boxes in topmost row
+(respectively), ls[1] is count for leftmost column, ls[2] count for bottom most
+row, and ls[3]. Each time one of these values of the box list is greater than
+the corresponding value in the goals list, then it's a stuck state.
+'''
+def is_stuck(gl, bx, chng):
+    if chng != 9:
+        bx[chng] += 1
+    for i in range(4):
+        if gl[i] < bx[i]:
+            return True, bx
+    return False, bx
 
 def main():
-    test, goals, _ = read()
+    test, goals, _, goals_ls, boxes = read()
     print(test, goals)
     print(is_cornered(test, goals, 1,3))
+
 
 
 if __name__ == "__main__":
